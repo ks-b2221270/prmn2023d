ec18faa3bafd0bf91c2d407b70085efc6dd6a82d
deckz <b2221270@photon.chitose.ac.jp>
2023-10-28 19:37:35 +0900

    プロジェクト作成

---
+++ prmn2023d/src/test/java/com/example/prmn2023d/Prmn2023dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prmn2023d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prmn2023dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
